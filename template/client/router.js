46e91b8d127d679e133014999fd941ab0aa4dc11
dingyiming <315129552@qq.com>
2016-09-27 16:47:31 +0800

    fix vue-router

--- template/client/router.js
+++ template/client/router.js
@@ -4,7 +4,9 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 // 引入页面
-const Home = r => require.ensure([], () => r(require('views/Home')), 'home');
+// const Home = r => require.ensure([], () => r(require('views/Home')), 'home');
+const Home = resolve => require(['views/Home'], resolve);
+
 
 export default new VueRouter({
   mode: 'hash',
