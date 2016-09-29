42d1c5460b35d3a1d0c23aa855f9da96861f70af
丁一鸣 <315129552@qq.com>
2016-09-29 22:33:19 +0800

    update dockerfile

--- template/server/main.js
+++ template/server/main.js
@@ -5,11 +5,11 @@ const compression = require('compression');
 
 app.use(compression());
 app.use(express.static(path.resolve('./')));
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendfile(path.resolve('./dist/html/index.html'));
 });
 
-var server = app.listen(10000, function () {
+var server = app.listen(80, function() {
   var host = server.address().address;
   var port = server.address().port;
 
