8a6883f7a7f16e003be1f0c3cc11f984c8750a69
dingyiming <315129552@qq.com>
2016-09-29 14:35:43 +0800

    add subdomain by dockerfile

--- template/server/main.js
+++ template/server/main.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendfile(path.resolve('./dist/html/index.html'));
 });
 
-var server = app.listen(10000, function () {
+var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
